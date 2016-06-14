39064c07f8f0033e87f3c1603c49b1ae841c06c1
Michael Galetzka <michael.galetzka@disy.net>
2016-06-14 09:00:57 -0700

    Added WKB and geoJSON exporter

---
+++ ExportHandler.h
@@ -0,0 +1,84 @@
+#pragma once
+
+#include <string>
+#include <algorithm>
+#include <fstream>
+#include "b64/base64.h"
+
+#include <osmium/geom/wkt.hpp>
+#include <osmium/geom/wkb.hpp>
+#include <osmium/geom/geojson.hpp>
+#include <osmium/handler.hpp>
+
+#define ELPP_NO_DEFAULT_LOG_FILE
+#include "easylogging++.h"
+
+using namespace std;
+using namespace osmium;
+
+class ExportHandler : public handler::Handler {
+
+private:
+    geom::WKTFactory<> wktFactory;
+    geom::WKBFactory<> wkbFactory;
+    geom::GeoJSONFactory<> gjsFactory;
+    fstream fs;
+
+    string toString(const TagList &list) {
+        string tags = "[";
+        for (auto &tag : list) {
+            tags.append(tag.key());
+            tags.append("=");
+            tags.append(tag.value());
+            tags.append(",");
+        }
+        tags += "]";
+        replace(tags.begin(), tags.end(), ';', ':'); // to protect the csv format
+        return tags;
+    }
+
+public:
+
+    ExportHandler() {
+        fs.open("result.csv", fstream::out);
+        fs << "id;type;tag;wkt\n";
+    }
+
+    void node(const Node &node) {
+        string wkt = wktFactory.create_point(node);
+        auto rawWkb = wkbFactory.create_point(node);
+        string wkb = base64_encode(rawWkb.c_str(), static_cast<int>(rawWkb.length()));
+        string gjs = gjsFactory.create_point(node);
+        fs << to_string(node.id()) << ";" << "node" << ";" << toString(node.tags()) << ";" << wkt << ";" << wkb <<
+        ";" << gjs << "\n";
+        LOG(TRACE) << 'n' << node.id() << ' ' << wkt << ";" << wkb << ";" << gjs;
+    }
+
+    void way(const Way &way) {
+        try {
+            string wkt = wktFactory.create_linestring(way);
+            auto rawWkb = wkbFactory.create_linestring(way);
+            string wkb = base64_encode(rawWkb.c_str(), static_cast<int>(rawWkb.length()));
+            string gjs = gjsFactory.create_linestring(way);
+            fs << to_string(way.id()) << ";" << "way" << ";" << toString(way.tags()) << ";" << wkt << ";" << wkb <<
+            ";" << gjs << "\n";
+            LOG(TRACE) << 'w' << way.id() << ' ' << wkt << ";" << wkb << ";" << gjs;
+        } catch (geometry_error &e) {
+            LOG(ERROR) << "Geometry with id " << way.id() << " is broken: " << e.what();
+        }
+    }
+
+    void area(const Area &area) {
+        try {
+            string wkt = wktFactory.create_multipolygon(area);
+            auto rawWkb = wkbFactory.create_multipolygon(area);
+            string wkb = base64_encode(rawWkb.c_str(), static_cast<int>(rawWkb.length()));
+            string gjs = gjsFactory.create_multipolygon(area);
+            fs << to_string(area.id()) << ";" << "area" << ";" << toString(area.tags()) << ";" << wkt << ";" << wkb <<
+            ";" << gjs << "\n";
+            LOG(TRACE) << 'a' << area.id() << ' ' << wkt << ";" << wkb << ";" << gjs;
+        } catch (geometry_error &e) {
+            LOG(ERROR) << "Geometry with id " << area.id() << " is broken: " << e.what();
+        }
+    }
+};
--- main.cpp
+++ main.cpp
@@ -3,12 +3,14 @@
 
 #include <osmium/area/assembler.hpp>
 #include <osmium/area/multipolygon_collector.hpp>
-#include <osmium/geom/wkt.hpp>
+
 #include <osmium/io/any_input.hpp>
 #include <osmium/index/map/sparse_mem_array.hpp>
 #include <osmium/handler/node_locations_for_ways.hpp>
 
 #include "LogginProblemReporter.h"
+#include "ExportHandler.h"
+
 INITIALIZE_EASYLOGGINGPP
 
 using namespace std;
@@ -17,68 +19,15 @@ using namespace osmium;
 typedef index::map::SparseMemArray<unsigned_object_id_type, Location> index_type;
 typedef handler::NodeLocationsForWays<index_type> location_handler_type;
 
-class ExportToWKTHandler : public handler::Handler {
-
-private:
-    geom::WKTFactory<> m_factory;
-    fstream fs;
-
-    string toString(const TagList &list) {
-        string tags = "[";
-        for (auto& tag : list) {
-            tags.append(tag.key());
-            tags.append("=");
-            tags.append(tag.value());
-            tags.append(",");
-        }
-        tags += "]";
-        replace(tags.begin(), tags.end(), ';', ':'); // to protect the csv format
-        return tags;
-    }
-
-public:
-
-    ExportToWKTHandler() {
-        fs.open("result.csv", fstream::out);
-        fs << "id;type;tag;wkt\n";
-    }
-
-    void node(const Node& node) {
-        string wkt = m_factory.create_point(node);
-        fs << to_string(node.id()) << ";" << "node" << ";" << toString(node.tags()) << ";" << wkt << "\n";
-        LOG(TRACE) << 'n' << node.id() << ' ' << wkt;
-    }
-
-    void way(const Way& way) {
-        try {
-            string wkt = m_factory.create_linestring(way);
-            fs << to_string(way.id()) << ";" << "way" << ";" << toString(way.tags()) << ";" << wkt << "\n";
-            LOG(TRACE) << 'w' << way.id() << ' ' << wkt;
-        } catch (geometry_error& e) {
-            LOG(ERROR) << "Geometry with id " << way.id() << " is broken: " << e.what();
-        }
-    }
-
-    void area(const Area& area) {
-        try {
-            string wkt = m_factory.create_multipolygon(area);
-            fs << to_string(area.id()) << ";" << "area" << ";" << toString(area.tags()) << ";" << wkt << "\n";
-            LOG(TRACE) << 'a' << area.id() << ' ' << wkt;
-        } catch (geometry_error& e) {
-            LOG(ERROR) << "Geometry with id " << area.id() << " is broken: " << e.what();
-        }
-    }
-};
-
-void exportWkt(string input_filename, area::MultipolygonCollector<area::Assembler>& collector) {
-    TIMED_SCOPE(timerObj, "exportWkt");
+void exportGeometry(string input_filename, area::MultipolygonCollector<area::Assembler>& collector) {
+    TIMED_SCOPE(timerObj, "exportGeometry");
 
     index_type index;
     location_handler_type location_handler(index);
-    ExportToWKTHandler export_handler;
+    ExportHandler export_handler;
 
-    io::Reader reader2(input_filename);
-    apply(reader2, location_handler, export_handler, collector.handler([&export_handler](const memory::Buffer& buffer) {
+    io::Reader reader(input_filename);
+    apply(reader, location_handler, export_handler, collector.handler([&export_handler](const memory::Buffer& buffer) {
         apply(buffer, export_handler);
     }));
 }
@@ -106,7 +55,7 @@ int main(int argc, char* argv[]) {
     io::Reader reader1(input_filename);
     collector.read_relations(reader1);
 
-    LOG(INFO) << "Converting file content to WKT...";
-    exportWkt(input_filename, collector);
+    LOG(INFO) << "Converting file content...";
+    exportGeometry(input_filename, collector);
     LOG(INFO) << "File " << argv[1] << " successfully converted (see result.csv).";
 }
